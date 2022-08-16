caca5aa2ebd240ef58924d744be1a0addca3327e
tonyglaz <tonyglazunov@gmail.com>
2022-08-16 16:22:59 +0300

    Зарегал модели для админки

--- orders_app/admin.py
+++ orders_app/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Order, Device, Customer, DeviceInField
 
-# Register your models here.
+
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'manufacturer', 'model')
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'device', 'customer', 'order_description', 'created_dt', 'last_updated_dt', 'order_status')
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer_name', 'customer_address', 'customer_city')
+
+
+class DeviceInFieldAdmin(admin.ModelAdmin):
+    list_display = ('id', 'serial_number', 'customer_id', 'analyzer_id', 'owner_status')
+
+
+# регистрирую класс модели для админки
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Device, DeviceAdmin)
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(DeviceInField, DeviceInField)
